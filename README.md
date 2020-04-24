440c2daed16046b9a7a934fa3759fc45adf8d516
tarun_srinivas <47295465+tarun231099@users.noreply.github.com>
2020-04-25 01:44:28 +0530

    Updated code
    Creates directories for each celeb and places images into each if found

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Bipolar-Intern
  Wiki Crawler
-This crawler is limited to crawl certain Wiki pages and get names, images and their hrefs accordingly. Images are stored elsewhere, info becomes a database.
+#This crawler is limited to crawl certain Wiki pages and get names, images and their hrefs accordingly. Images are stored elsewhere, info becomes a database.
+#We use Yolo Models to clean the data, taking only images which have people in it, wiki can have a lot of unwanted images.
+Since YOLO model is kind of a transfer learning model, we have to download the weights, which cannot be uploaded on github, 
+## You can download the YOLO weights here : "https://pjreddie.com/media/files/yolov3-spp.weights"
--- Spider1.py
+++ Spider1.py
@@ -3,6 +3,10 @@ from bs4 import BeautifulSoup
 import numpy as np 
 import mimetypes
 import os
+from model import detect
+import cv2 
+from skimage import io
+
 def names():
 	website = "https://en.wikipedia.org/wiki/List_of_Indian_film_actors"
 	result  = requests.get(website)
@@ -18,53 +22,73 @@ def names():
 	for div in data:	
 		links = div.findAll('a')
 		for link in links:
-			href = 'https://en.wikipedia.org/' + link.get('href')
-			hrefs.append(href)
+			
 			title = link.get('title')
 			if(str(link.string) in str(title)):
 			#print("sup")
 				name = link.string
 				names.append(name)
+				href = 'https://en.wikipedia.org/' + link.get('href')
+				hrefs.append(href)
 				#print(name)
 	names = np.asarray(names)
 	hrefs = np.asarray(hrefs)
 	print(names.shape)
-	return hrefs
-def imgs():
+	print(hrefs.shape)
+	return hrefs,names
+def imgs(href):
 	imgs = []
-	i=1
-	links = names()
-	for href in links:
-		img_search = requests.get(href)
-		img_soup = BeautifulSoup(img_search.content,"html.parser")
-		for img_link in img_soup.findAll('img'):
+	lock=1
+	#links,name = names()
+	#for href in links:
 
-			while(img_link.get('height')):
-				if int(img_link.get('height')) > 100:
-					img_src = 'https:' + img_link.get('src')
-					imgs.append(img_src)
-				break
+	img_search = requests.get(href)
+	img_soup = BeautifulSoup(img_search.content,"html.parser")
+	for img_link in img_soup.findAll('img'):
 
-		print(i)
-		i=i+1
-		#if i == 3:
-		#	break
+		while(img_link.get('height')):
+
+			if int(img_link.get('height')) > 100:
+				img_src = 'https:' + img_link.get('src')
+				imgs.append(img_src)
+			break
+
+	print(lock)
+	#lock=lock+1
+	#if lock == 2:
+	#break
 	imgs = np.asarray(imgs)
 	print(imgs.shape)
 	return imgs
 def file_write():
-	locs = imgs()
+	links,name = names()
+	
 	total = 1
 
-	for i in range(len(locs)):
-		r = requests.get(locs[i])
-		typ = r.headers['content-type']
-		ext = mimetypes.guess_extension(typ)
-		p = "C:\\Users\\Asus\\Desktop\\Images\\"+str(total).zfill(4)+ext
-		file = open(p,'wb')
-		file.write(r.content)
-		file.close()
-		#print(total)
-		total = total +1
+	for i in range(10):
+		locs = imgs(links[i])
+		p = "C:\\Users\\Asus\\Desktop\\Images\\" + name[i] +"\\"
+		os.mkdir(p)
+		for j in range(len(locs)):
+			r = requests.get(locs[j])
+			print (locs[j])
+			img = io.imread(locs[j])
+			img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)   #scikit image reads in bgr
+
+			model = detect.model()							#importing model
+			result = detect.predict(img,model)	
+			print(result)
+
+			if 'person' in result:
+				typ = r.headers['content-type']
+				ext = mimetypes.guess_extension(typ)
+				p = p +str(total).zfill(4)+ext
+				file = open(p,'wb')
+				file.write(r.content)
+				file.close()
+				print(total)
+				total = total +1
+			else:
+				print("Bad Image Found, Ignoring")
 
 file_write()
